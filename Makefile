0a96378b7f133aa9a283288983b654552e5cf318
Ruslan Akbashev <ruslashev@gmail.com>
2015-11-23 02:57:09 +0300

    start decoding

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	g++ decode.cc -o decode
+	g++ decode.cc -o decode -std=c++14
 	./decode
-	g++ encode.cc -o encode -std=c++14
-	./encode
+	# g++ encode.cc -o encode -std=c++14
+	# ./encode
--- decode.cc
+++ decode.cc
@@ -1,16 +1,28 @@
 #include <fstream>
 #include <cstdarg>
 #include <cstdlib>
+#include <vector>
+#include <deque>
 
-void die(const char *format, ...);
+void die(const char *format, ...)
+{
+  va_list args;
+  va_start(args, format);
+  vprintf(format, args);
+  va_end(args);
+  puts("");
+  exit(1);
+}
 
 struct tree
 {
   tree *left, *right;
-  int val, freq;
+  bool leaf;
+  char val;
+  unsigned int freq;
 };
 
-unsigned char *file;
+std::deque<char> bits;
 
 void readfile()
 {
@@ -20,10 +32,24 @@ void readfile()
   fseek(fd, 0, SEEK_END);
   size_t len = ftell(fd);
   rewind(fd);
-  file = new unsigned char [len+1];
+  unsigned char *file = new unsigned char [len+1];
   if (fread(file, 1, len, fd) != len)
     die("failed to read file \"in\"");
   fclose(fd);
+
+  for (size_t i = 0; i < len; i++) {
+    unsigned char byte = file[i];
+    std::deque<char> bitsrev;
+    printf("current byte: %d\n", byte);
+    for (size_t j = 0; j < 8; j++) {
+      bitsrev.push_front(byte % 2);
+      byte /= 2;
+    }
+    for (char &c : bitsrev) {
+      printf("%d\n", c);
+      bits.push_back(c);
+    }
+  }
 }
 
 void readtree()
@@ -36,16 +62,6 @@ int main()
   readfile();
   readtree();
   return 0;
-}
-
-void die(const char *format, ...)
-{
-  va_list args;
-  va_start(args, format);
-  vprintf(format, args);
-  va_end(args);
-  puts("");
-  exit(1);
 }
 
 // vim: et:ts=2:sw=2
